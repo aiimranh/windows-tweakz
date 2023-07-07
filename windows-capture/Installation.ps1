9218defd8d31ea498cad1a1faf63d7269142e31e
MD. IMRAN <59414567+aiimranh@users.noreply.github.com>
2023-07-08 00:00:18 +0600

    Update Installation.ps1

--- windows-capture/Installation.ps1
+++ windows-capture/Installation.ps1
@@ -2,5 +2,5 @@
 Add-ProvisionedAppxPackage -Online -PackagePath "C:\Users\$Env:UserName\Downloads\MicrosoftHEVCVideoExtension\Microsoft.HEVCVideoExtension.Appx" -SkipLicense
 
 # Installing Office 365
-"C:\Users\$Env:UserName\Downloads\Office\setup.exe" /download "C:\Users\$Env:UserName\Downloads\Office\Configuration.xml"
-"C:\Users\$Env:UserName\Downloads\Office\setup.exe" /configure "C:\Users\$Env:UserName\Downloads\Office\Configuration.xml"
+"$pwd\Office\setup.exe" /download "$pwd\Office\Configuration.xml"
+"$pwd\Office\setup.exe" /configure "$pwd\Office\Configuration.xml"
